01c37ffff6e6b3bfb48557546cd1340e0ee5fe77
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2022-11-09 10:56:24 -0500

    Fix dark mode font color bug

--- script-light-dark-mode.js
+++ script-light-dark-mode.js
@@ -1,8 +1,10 @@
 const bdark = document.getElementById('bdark')
 const body = document.querySelector('body')
 const header = document.querySelector('header')
+const main = document.querySelector('main')
 
 bdark.addEventListener('click', function(){
     body.classList.toggle('dark-mode')
     header.classList.toggle('dark-mode')
+    main.classList.toggle('dark-mode')
 })
